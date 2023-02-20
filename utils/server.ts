25c6a1ce37671f184cb57830fc9505877c8323ca
Wolf-Quiteque <marcioqui3@gmail.com>
2023-02-20 14:14:28 +0100

    Jk

--- utils/server.ts
+++ utils/server.ts
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev
   ? "http://localhost:3000"
-  : "https://loreal-ao.vercel.app/";
+  : "https://loreal-ao.vercel.app";
